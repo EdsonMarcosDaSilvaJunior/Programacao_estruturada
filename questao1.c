d4b9571b33cccaa7176017bac4b5bf4dd9312e67
Edson Junior <edsonmsilvajr@gmail.com>
2024-09-12 19:26:43 -0300

    feat: adiciona funcionamento ao codigo e finaliza a questao 1

--- questao1.c
+++ questao1.c
@@ -7,11 +7,31 @@ void DesenhaLinha(int qtd) {
 
 int Intervalo(int menor, int maior){
     int i, soma = 0;
-    for(i = 0; menor < maior; i++){
-        soma += 1;
+    for(i = menor; i <= maior; i++){
+        soma += i;
     }
 
     return soma;
 }
 
-int main (){ }
+int main (){
+    int menor, maior = 0;
+    int linha, esc = 0;
+
+    printf("Tamanho da linha: ");
+    scanf("%d", &linha);
+
+    do{
+        printf("Digite o menor numero: ");
+        scanf("%d", &menor);
+        printf("Digite o maior numero: ");
+        scanf("%d", &maior);
+        DesenhaLinha(linha);
+
+        Intervalo(menor, maior);
+
+        printf("\nA soma dos intervalos eh: %d", Intervalo(menor, maior));
+        printf("\nVoce deseja continuar? ");
+        scanf("%d", &esc);
+    } while (esc != 0);
+ }
