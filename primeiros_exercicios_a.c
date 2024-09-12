2e504f0cba24a39a7303fbbb6d9bb6a15021c47d
Edson Junior <edsonmsilvajr@gmail.com>
2024-09-12 20:10:39 -0300

    rm: remove gambiarra

--- primeiros_exercicios_a.c
+++ primeiros_exercicios_a.c
@@ -1,30 +1,30 @@
-int main() {
-    int numeros [10], i = 0, maior = -999, posMaior = 0, menor = 999, posMenor = 0;
+int main()
+{
+    int numeros[10], i = 0, maior = -999, posMaior = 0, menor = 999, posMenor = 0;
     int esc = 0;
-   
-    
-    
 
-    do{
-         for(i = 0; i < 10; i++){
-        printf("Digite o %d numero do vetor: ", i+1);
+    for (i = 0; i < 10; i++)
+    {
+        printf("Digite o %d numero do vetor: ", i + 1);
         scanf("%d", &numeros[i]);
     }
-    
-
 
     printf("Seu vetor eh: ");
-    for(i = 0; i < 10; i++){
+    for (i = 0; i < 10; i++)
+    {
         printf("%d", numeros[i]);
     }
     printf("\n");
 
-    for(i = 0; i < 10; i++){
-        if(numeros[i] > maior) {
+    for (i = 0; i < 10; i++)
+    {
+        if (numeros[i] > maior)
+        {
             maior = numeros[i];
             posMaior = i;
         }
-        if(numeros[i] < menor){
+        if (numeros[i] < menor)
+        {
             menor = numeros[i];
             posMenor = i;
         }
@@ -32,9 +32,6 @@ int main() {
 
     printf("\nO maior numero eh: %d, posicao: %d", maior, posMaior);
     printf("\nO menor numero eh: %d, posicao: %d", menor, posMenor);
-        printf("\nVoce deseja continuar? ");
-        scanf("%d", &esc);
-    } while (esc != 0);
 
     return 0;
 }
