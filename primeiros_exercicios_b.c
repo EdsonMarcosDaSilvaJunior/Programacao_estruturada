0e36695e395b6858262c7d40ecba94deb8356271
Edson Junior <edsonmsilvajr@gmail.com>
2024-09-12 21:57:14 -0300

    feat: adiciona calculaBissexto

--- primeiros_exercicios_b.c
+++ primeiros_exercicios_b.c
@@ -7,23 +7,45 @@ int CalculaDias(int anoN, int anoA)
     return test;
 }
 
+int CalculaBissexto(ano)
+{
+    if (ano % 4 == 0)
+    {
+        if (ano % 100 != 0){
+            return 1;
+        }else if (ano % 400 == 0)
+        {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
+
 int main()
 {
     char nome[100] = {0};
     int anoN = 0, anoA = 0;
     int esc = 0;
 
-    printf("Digite seu nome: ");
-    scanf(" %s", &nome);
+    do
+    {
 
-    printf("Digite seu ano de nascimento: ");
-    scanf("%d", &anoN);
+        printf("Digite seu nome: ");
+        scanf(" %s", &nome);
 
-    printf("Digite o ano atual: ");
-    scanf("%d", &anoA);
+        printf("Digite seu ano de nascimento: ");
+        scanf("%d", &anoN);
 
-    printf("\nTotal de dias vividos: %d", CalculaDias(anoN, anoA));
+        printf("Digite o ano atual: ");
+        scanf("%d", &anoA);
 
-    printf("\nVoce deseja continuar? ");
-    scanf("%d", &esc);
+        printf("\nTotal de dias vividos: %d", CalculaDias(anoN, anoA));
+        printf("Calcula bissexto %d", CalculaBissexto(anoN));
+
+        printf("\nVoce deseja continuar? ");
+        scanf("%d", &esc);
+    } while (esc != 0);
 }
