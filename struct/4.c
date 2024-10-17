f58664982bbe50982176481ab80e9746e4a17d1e
Edson Junior <edsonmsilvajr@gmail.com>
2024-10-17 19:54:43 -0300

    Corrige atividade 4

--- struct/4.c
+++ struct/4.c
@@ -1,11 +1,12 @@
 typedef struct 
 {
-    int matricula, nota1, nota2, nota3;
+    int matricula;
+    float nota1, nota2, nota3;
     char nome[100];
 } Aluno;
 
 void EncontraMaiorNotaProva1(Aluno alunos[], int qtdAlunos){
-    int maiorNota = -999;
+    float maiorNota = -999;
     Aluno alunoMaiorNota;
     for(int i = 0; i < qtdAlunos; i++){
         if(alunos[i].nota1 > maiorNota){
@@ -14,46 +15,46 @@ void EncontraMaiorNotaProva1(Aluno alunos[], int qtdAlunos){
         }
     }
 
-    printf("\nO aluno com a maior nota eh: %s com a nota: %d", alunoMaiorNota.nome, alunoMaiorNota.nota1);
+    printf("\nO aluno com a maior nota eh: %s com a nota: %.2f.\n", alunoMaiorNota.nome, alunoMaiorNota.nota1);
 }
 
 void MaiorMedia(Aluno alunos[], int qtdAlunos){
-    int maiorMedia = -999;
+    float maiorMedia = -999;
     Aluno alunoMaiorMedia;
 
     for(int i = 0; i < qtdAlunos; i++){
-        int media = alunos[i].nota1 + alunos[i].nota2 + alunos[i].nota3 / 3;
+        float media = (alunos[i].nota1 + alunos[i].nota2 + alunos[i].nota3) / 3;
         if(media > maiorMedia){
             maiorMedia = media;
             alunoMaiorMedia = alunos[i];
         }
     }
     
-    printf("\nO aluno com a maior media eh: %s com a media: %2.d", alunoMaiorMedia.nome, alunoMaiorMedia.nota1 + alunoMaiorMedia.nota2 + alunoMaiorMedia.nota3 / 3);
+    printf("\nO aluno com a maior media eh: %s com a media: %.2f.\n", alunoMaiorMedia.nome, (alunoMaiorMedia.nota1 + alunoMaiorMedia.nota2 + alunoMaiorMedia.nota3) / 3);
 }
 
 void MenorMedia(Aluno alunos[], int qtdAlunos){
-    int maiorMedia = 999;
+    float maiorMedia = 999;
     Aluno alunoMenorMedia;
 
     for(int i = 0; i < qtdAlunos; i++){
-        int media = alunos[i].nota1 + alunos[i].nota2 + alunos[i].nota3 / 3;
+        float media = (alunos[i].nota1 + alunos[i].nota2 + alunos[i].nota3) / 3;
         if(media < maiorMedia){
             maiorMedia = media;
             alunoMenorMedia = alunos[i];
         }
     }
     
-    printf("\nO aluno com a menor media eh: %s com a media: %2.d", alunoMenorMedia.nome, alunoMenorMedia.nota1 + alunoMenorMedia.nota2 + alunoMaiorMedia.nota3 / 3);
+    printf("\nO aluno com a menor media eh: %s com a media: %.2f\n", alunoMenorMedia.nome, (alunoMenorMedia.nota1 + alunoMenorMedia.nota2 + alunoMenorMedia.nota3) / 3);
 }
 
-void aprovado(Aluno alunos[], int qtdAlunos){
+void Aprovado(Aluno alunos[], int qtdAlunos){
     for(int i = 0; i < qtdAlunos; i++){
-        int media = alunos[i].nota1 + alunos[i].nota2 + alunos[i].nota3 / 3;
+        float media = (alunos[i].nota1 + alunos[i].nota2 + alunos[i].nota3) / 3;
         if(media > 6){
-           printf("Aprovado!!");
+           printf("\nAluno: %s. Matricula: %d. Aprovado!!\n", alunos[i].nome, alunos[i].matricula);
         } else {
-            printf("Reprovado!!");
+            printf("\nAluno: %s. Matricula: %d. Reprovado!!\n", alunos[i].nome, alunos[i].matricula);
         }
     }
 }
@@ -71,11 +72,11 @@ int main(){
             printf("Digite sua matricula: ");
             scanf("%d", &alunos[i].matricula);
             printf("Digite nota primeira Prova: ");
-            scanf(" %d", &alunos[i].nota1);
+            scanf("%f", &alunos[i].nota1);
             printf("Digite nota segunda Prova: ");
-            scanf(" %d", &alunos[i].nota2);
+            scanf("%f", &alunos[i].nota2);
             printf("Digite nota terceira Prova: ");
-            scanf(" %d", &alunos[i].nota3);
+            scanf("%f", &alunos[i].nota3);
 
             printf("\nAluno %d cadastrado!!\n", i+1);
         }
@@ -83,6 +84,7 @@ int main(){
         EncontraMaiorNotaProva1(alunos, qtdAlunos);
         MaiorMedia(alunos, qtdAlunos);
         MenorMedia(alunos, qtdAlunos);
+        Aprovado(alunos, qtdAlunos);
 
 
     } while(1!=0);
