544856a9cc8b183d8f134149bcd97aaa4d45fcbe
Edson Junior <edsonmsilvajr@gmail.com>
2024-10-17 21:20:10 -0300

    Termina Atividade 9

--- struct/9.c
+++ struct/9.c
@@ -4,15 +4,38 @@ typedef struct {
     float media; 
 } Aluno;
 
-void Situaçao(Aluno alunos[]){
-    
-}
+void Situacao(Aluno alunos[]){
+    Aluno alunosAprovados[10];
+    Aluno alunosReprovados[10];
+
+    int aprovado = 0;
+    int reprovado = 0;
+    for(int i = 0; i < 3; i++){
+        if(alunos[i].media > 5.0){
+            alunosAprovados[aprovado] = alunos[i];
+            aprovado++;
+        } else {
+            alunosReprovados[reprovado] = alunos[i];
+            reprovado++;
+        }
+    }
+
+        printf("\nAlunos Aprovados: \n");
+        for(int i = 0; i < aprovado; i++){
+            printf("\nNome: %s, Matricula: %d, Media: %.2f\n", alunosAprovados[i].nome, alunosAprovados[i].matricula, alunosAprovados[i].media);
+        }
+
+        printf("\nAlunos Reprovados: \n");
+        for(int i = 0; i < reprovado; i++){
+            printf("\nNome: %s, Matricula: %d, Media: %.2f\n", alunosReprovados[i].nome, alunosReprovados[i].matricula, alunosReprovados[i].media);
+        }
+};
 
 int main(){
-    Aluno alunos[10];
+    Aluno alunos[3];
 
     do {
-      for(int i = 0; i < 10; i++){
+      for(int i = 0; i < 3; i++){
         printf("Digite o nome do aluno: ");
         scanf(" %s", &alunos[i].nome);
 
@@ -23,7 +46,7 @@ int main(){
         scanf("%f", &alunos[i].media);
       }
 
-
+      Situacao(alunos);
 
 
     } while(1!=0);
